28f4983e5581ea66d7add0acc9cd69126468fc57
Athiraks412 <athiraks244@gmail.com>
2024-11-20 15:27:56 +0530

    first commit

---
+++ src/arraydemopkg/demo.java
@@ -0,0 +1,23 @@
+package arraydemopkg;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num[]= {1,2,3,4,5,6,7,8,9,10};
+		//System.out.println(num[1]);
+	
+		/*for (int i=0;i<num.length;i++)
+		{
+			System.out.println (num[i]);
+		}*/
+		
+		for (int i:num)
+		{
+			System.out.println(i);
+			
+		}
+	}
+
+}
